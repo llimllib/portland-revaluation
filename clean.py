c165a864c1ae4afd46a0d49fe9372c06322d06d6
Bill Mill <bill@billmill.org>
2021-07-13 13:15:22 -0400

    hacking

--- clean.py
+++ clean.py
@@ -1,29 +1,84 @@
+#!/usr/bin/env
 import json
-import os
+import re
 
-import mapbox
+
+def tryn(maybe_n):
+    try:
+        n = int(maybe_n)
+        return n
+    except ValueError:
+        try:
+            n = float(maybe_n)
+            return n
+        except ValueError:
+            return maybe_n
+
+
+def remove_whitespace(s):
+    return re.sub(r"\s", "", s)
+
+
+def dictize(list_of_pairs):
+    data = {}
+    for kv in list_of_pairs:
+        if kv == [""]:
+            continue
+        k, v = kv
+        try:
+            k = remove_whitespace(k)
+        except TypeError:
+            print(k, type(k))
+            raise
+
+        # if the value has nothing but whitespace, set it to empty if there's
+        # a key, then otherwise skip ahead
+        if not remove_whitespace(v):
+            if k:
+                data[k] = ""
+            continue
+
+        # skip the "verify with legal" rows
+        if v.startswith("Verify"):
+            continue
+
+        if k:
+            data[k] = tryn(v)
+            prev_k = k
+        else:
+            # if there wasn't a key, add this line to the previous key.
+            # Convert to list if necessary
+            if isinstance(data[prev_k], list):
+                data[prev_k].append(tryn(v))
+            else:
+                data[prev_k] = [data[prev_k], tryn(v)]
+
+    return data
+
 
 property_data = json.load(open("property_data.json"))
 
 cleaned = {}
-
 errors = 0
-for parcel, data in property_data.items():
+for parcel_id, data in property_data.items():
     # I don't have a good data type for errors, so assume it's an error if
     # assessments isn't in there
-    if 'assessments' not in parcel:
+    if "assessments" not in data:
         errors += 1
         continue
 
-    assessments = {}
-    for year, land, building, total, std_exemption, other_exemption, taxable_value in parcel['assessments'][1:]:
-        assessments[year] = {
-            "land": land,
-            "building": building,
-            "total": total,
-            "std_exemption": std_exemption,
-            "other_exemption": other_exemption,
-            "taxable_value": taxable_value,
-        }
+    cleaned[parcel_id] = {}
 
+    cleaned[parcel_id]["assessments"] = list(
+        filter(lambda y: len(y) == 7, data["assessments"][1:])
+    )
 
+    cleaned[parcel_id]["parcelData"] = dictize(data["parcelData"])
+    cleaned[parcel_id]["ownerData"] = dictize(data["ownerData"])
+    # I'm going to do this in a separate script
+    # cleaned[parcel_id]["geo"] = geocode(
+    #     cleaned[parcel_id]["parcelData"]["PropertyLocation"]
+    # )
+
+print(f"{errors} errors, {len(property_data)-errors} nominal")
+json.dump(cleaned, open("cleaned_data.json", "w"), indent=2)
---
+++ geocode.py
@@ -0,0 +1,52 @@
+import json
+import os
+import time
+
+import mapbox
+
+MAPBOX_API_TOKEN = os.environ["MAPBOX_API_TOKEN"]
+
+GEOCODE = mapbox.Geocoder(access_token=os.environ["MAPBOX_API_TOKEN"])
+
+
+def geocode(addr):
+    res = GEOCODE.forward(f"{addr}, Portland, ME")
+    if res.headers.get("X-Rate-Limit-Limit") == "0":
+        deltatime = res.headers["X-Rate-Limit-Reset"] - time.time()
+        print("sleeping", deltatime)
+        time.sleep(deltatime)
+
+    if res.ok:
+        data = res.json()
+        if len(data["features"]) == 0:
+            return {"error": "Unable to geocode"}
+
+        feat = data["features"][0]
+        return {
+            "lat": feat["center"][0],
+            "lng": feat["center"][1],
+            "full_name": feat["place_name"],
+        }
+    else:
+        import ipdb
+
+        ipdb.set_trace()
+
+
+data = json.load(open("cleaned_data.json"))
+if os.path.exists("geodata.json"):
+    geodata = json.load(open("geodata.json"))
+else:
+    geodata = {}
+n = 0
+for parcel_id, data in data.items():
+    if parcel_id in geodata:
+        continue
+    geodata[parcel_id] = data
+    geodata[parcel_id]["geo"] = geocode(
+        geodata[parcel_id]["parcelData"]["PropertyLocation"]
+    )
+    json.dump(geodata, open("geodata.json", "w"), indent=2)
+    n += 1
+    if n % 100 == 0:
+        print(n)

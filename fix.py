86bc1a43c22adfdfba8ff383c898c88332c14b86
Bill Mill <bill@billmill.org>
2021-07-15 09:31:52 -0400

    remove fix script

--- fix.py
+++
@@ -1,9 +0,0 @@
-import json
-
-geodata = json.load(open("geodata.json"))
-for parcel, data in geodata.items():
-    data["geo"]["lng"], data["geo"]["lat"] = (
-        data["geo"]["lat"],
-        data["geo"]["lng"],
-    )
-json.dump(geodata, open("geodata.json", "w"), indent=2)
